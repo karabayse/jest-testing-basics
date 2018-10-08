a11ca2e39b361a8423e97cba6f7d6d8ac395e14c
Kara Bayse <karabayse@gmail.com>
2018-10-08 14:54:28 -0500

    added test for null

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,18 @@ const functions = require('./functions');
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});

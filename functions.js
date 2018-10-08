0442d87f2356b428bb7371c9fb5de0c444c98074
Kara Bayse <karabayse@gmail.com>
2018-10-08 15:12:09 -0500

    added toEqual

--- functions.js
+++ functions.js
@@ -1,7 +1,12 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
-  checkValue: x => x
+  checkValue: x => x,
+  createUser: () => {
+    const user = { firstName: 'Kara'}
+    user['lastName'] = 'Bayse';
+    return user;
+  }
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,11 @@
 const functions = require('./functions');
 
+// toBe
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
+// toBe (does not work on an object or an array)
 test('Adds 2 + 2 to NOT equal 5', () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
@@ -19,7 +21,18 @@ test('Should be null', () => {
   expect(functions.isNull()).toBeNull();
 });
 
-// Test passes, because null is falsy 
+// Test passes, because null is falsy
 test('Should be falsy', () => {
   expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// Test fails, because 'toBe' is for primitive types whereas
+// an object or an array is a reference type in JS  ->  should use 'toEqual'
+test('User should be Kara Bayse object', () => {
+  expect(functions.createUser()).toBe({ firstName: 'Kara', lastName: 'Bayse'});
+});
+
+// toEqual
+test('User should be Kara Bayse object', () => {
+  expect(functions.createUser()).toEqual({ firstName: 'Kara', lastName: 'Bayse'});
 });
